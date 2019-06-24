4b2e72b062db64c35d6a53484d0d1d6ab2b86b3b
Soumil07 <soumil.paranjpay@gmail.com>
2019-06-24 20:42:52 +0530

    [Feat] Different env files for production and development

--- src/lib/Aero.js
+++ src/lib/Aero.js
@@ -1,6 +1,6 @@
 const { Client } = require('klasa');
 
-const { klasa, discord, aero } = require('../../config/index');
+const { klasa, discord } = require('../../config/index');
 const Permissions = require('./Permissions');
 require('./extensions/Message');
 require('./settings');
@@ -12,11 +12,6 @@ module.exports = class Aero extends Client {
 		super({ ...klasa, ...discord });
 
 		this.permissions = new Permissions(this);
-	}
-
-	login() {
-		const token = process.env[`DISCORD_TOKEN${aero.production ? '' : '_DEV'}`];
-		super.login(token);
 	}
 
 };
