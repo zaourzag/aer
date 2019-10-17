8586476adcec8edcc913d91352bc8b20c161af3a
Raven <marinus@reimer.world>
2019-10-17 16:07:32 +0200

    cq improvements

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 const { readdirSync } = require('fs');
-const { join } = require('path');
 
-for (const file of readdirSync(join(process.cwd(), 'config'))) {
+for (const file of readdirSync(__dirname)) {
 	if (!(file === 'index.js') && file.endsWith('.js')) {
 		module.exports[file.split('.')[0]] = require(`./${file}`);
 	}
