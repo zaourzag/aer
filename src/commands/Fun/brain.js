fae9688d0236039270a5f49ced6ac734240a93a3
Raven <marinus@reimer.world>
2019-11-05 23:09:09 +0100

    move away from imgflip

--- src/commands/Fun/brain.js
+++ src/commands/Fun/brain.js
@@ -14,21 +14,12 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [...sentences]) {
-		const { json } = await req('https://api.imgflip.com/caption_image')
-			.query({
-				username: process.env.IMGFLIP_USER,
-				password: process.env.IMGFLIP_PASS,
-				template_id: BRAIN_MEME_ID // eslint-disable-line camelcase
-			})
-			.query(
-				sentences.reduce((obj, item, i) => {
-					obj[`boxes[${i}][text]`] = encodeURIComponent(item);
-					return obj;
-				}, {})
-			)
+		const { body } = await req(this.client.config.memegenURL)
+			.header('Authorization', process.env.MEMEGEN_TOKEN)
+			.path('brain')
+			.query('text', sentences.join(', '))
 			.send();
-		if (!json.success) throw msg.language.get('ERROR_GENERIC', json.error_message);
-		return msg.channel.sendFile(json.data.url);
+		return msg.channel.sendFile(body);
 	}
 
 };
