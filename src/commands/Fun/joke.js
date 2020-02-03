24fa0559f3de0528239882ac79f6c3b8b4348fb5
Zakaria aourzag <z.aourzag@gmail.com>
2020-02-03 20:55:29 +0100

    Update joke.js

--- src/commands/Fun/joke.js
+++ src/commands/Fun/joke.js
@@ -1,14 +1,14 @@
-const axios = require("axios")
+const req = require('@aero/centra')
 const Discord = require("discord.js")
 async
 if (command === "joke"){
-let getjoke - async () => {let response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+let getjoke - async () => {let response = await req('https://official-joke-api.appspot.com/random_joke').send().then(res => res.json) ;
 
 let joke = response.data 
 return joke
 
-}
+}{
 let jokevalue = await getjoke();
-msg.reply(`here's your joke \n $jokevalue.setup \n \n $jokevalue.punchline`)
+msg.reply(`here's your joke \n $jokevalue.setup \n \n $jokevalue.punchline`);
 
 }
