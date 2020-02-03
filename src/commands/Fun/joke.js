fda524e9fe04a9eaf64e029c7aea71b1ff3a3611
Zakaria aourzag <z.aourzag@gmail.com>
2020-02-03 22:49:06 +0100

    Update joke.js

--- src/commands/Fun/joke.js
+++ src/commands/Fun/joke.js
@@ -6,7 +6,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			//aliases: ['joke'],
+		
 			description: language => language.get('COMMAND_JOKE_DESCRIPTION')
 		});
 
